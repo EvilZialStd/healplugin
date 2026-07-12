e4abe4b3f8550de18ed7cc0d18cb6d2d9e1e1137
Evil_ZialStd <ZialStdNeserit@yandex.ru>
2026-07-12 21:42:16 +0300

    Add HealPlugin source code

--- src/main/java/me/evilzialstd/healPlugin/HealPlugin.java
+++ src/main/java/me/evilzialstd/healPlugin/HealPlugin.java
@@ -1,16 +1,35 @@
 package me.evilzialstd.healPlugin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class HealPlugin extends JavaPlugin {
+public final class HealPlugin extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("heal").setExecutor(this);
+        getLogger().info("enabled.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("disabled.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!cmd.getName().equalsIgnoreCase("heal")) {
+            return true;
+        }
+
+        if (sender instanceof Player player) {
+            player.setHealth(20.0);
+            player.sendMessage("You`re healed.");
+        }
+
+        return true;
     }
 }
